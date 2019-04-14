9ab1f7023ed4a6b73c9d13ba2122be1381fbf515
Wera Barinova <alisawera@gmail.com>
2019-04-14 11:10:37 +0300

    template for fenster added

--- fenster/views.py
+++ fenster/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Das fenster is 'the window' in German")
+    template = loader.get_template('fenster/index.html')
+    return HttpResponse(template.render())
+
