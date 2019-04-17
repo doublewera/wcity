5616c28487c1198f2a21cf1fe7e0c8e2362b0389
Wera Barinova <alisawera@gmail.com>
2019-04-17 17:03:32 +0300

    "database objects added; Random creation and insertion made"

--- fenster/models.py
+++ fenster/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Fenster(models.Model):
+    fenster_width = models.IntegerField()
+    window_view = models.CharField(default='', max_length=1024)
+
+class Apt(models.Model):
+    fenster = models.ForeignKey(Fenster, on_delete=models.CASCADE)
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
+    altitude = models.FloatField(null=False)
+
--- fenster/views.py
+++ fenster/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
+from .models import Fenster
+from random import randint
+
 def index(request):
+    test_creation()
+    fenster_list = Fenster.objects.order_by("id")
     context = {
         "window_height": 100,
-        "window_width": 50,
-        "fenstertypes": [True, True, False, None, ""],
+        "window_width": fenster_list[0].fenster_width,
+        "fenstertypes": [True, False],
+        "how_many_fenster": len(fenster_list)
     }
     return render(request, 'fenster/index.html', context)
+
+def test_creation():
+# Create a new Fenster.
+    f = Fenster(
+        fenster_width=randint(100,256),
+        window_view=''
+    )
+    f.save()
 
