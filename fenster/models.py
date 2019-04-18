94d7073156e10d6af1efef98029feb57cf83a6bf
Wera Barinova <alisawera@gmail.com>
2019-04-18 10:43:15 +0300

    Good window model

--- fenster/models.py
+++ fenster/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.core.validators import int_list_validator
 
 # Create your models here.
 
 class Fenster(models.Model):
-    fenster_width = models.IntegerField()
+    fenster_width = models.IntegerField(default=50)
+    fenster_height = models.IntegerField(default=50)
+    fenster_scheme = models.CharField(
+        validators=[int_list_validator],
+        default='1,2',
+        max_length=1024
+    )
     window_view = models.CharField(default='', max_length=1024)
 
 class Apt(models.Model):
