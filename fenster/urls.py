c632d267c633a1ba2b9d206291da4d275298c33e
Wera Barinova <alisawera@gmail.com>
2019-04-18 12:37:18 +0300

    "We can buy a fenster now!"

--- fenster/urls.py
+++ fenster/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.index, name='buy'),
+    #path('<int:fenster_id>/', views.buy, name='buy'),
+    # по 'name' можно вызвать функцию в шаблоне по тэгу {% url %} 
 ]
--- fenster/views.py
+++ fenster/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Fenster
 from random import randint
 
 def index(request):
-    test_creation()
-    fenster_list = Fenster.objects.order_by("id")
+    #test_creation()
     context = {
-        "window_height": 100,
-        "window_width": fenster_list[0].fenster_width,
-        "fenstertypes": [True, False],
-        "how_many_fenster": len(fenster_list)
+        "request_place": str(request)
     }
+    try:
+        if 'selected_fenster' in request.POST:
+            fenster_id = request.POST['selected_fenster']
+            return buy(request, fenster_id)
+        context["request_place"] += str(request.POST)
+    except Exception as e:
+        context["an_exception"] = str(e) + str(type(e))
+    return display_all(request, context)
+
+def buy(request, fenster_id):
+    all_fensters = Fenster.objects
+    Fenster.objects.filter(id=fenster_id).delete()
+    return display_all(request)
+
+def display_all(request, context={}):
+    fenster_list = Fenster.objects.order_by("id")
+    context["fenster_list"] = fenster_list
     return render(request, 'fenster/index.html', context)
 
 def test_creation():
